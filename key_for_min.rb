943b49907804900803c08918b4dcb5b701a2ad7d
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-18 18:49:26 -0500

    Add files via upload

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    new_array = []
+    name_hash.each do |key, value|
+        new_array << value
+    end
+    lowest_value = new_array[0]
+    new_array.each do |i|
+        if i < lowest_value
+            lowest_value = i
+        end
+    end
+    name_hash.each do |key, value|
+        if value == lowest_value
+        return key
+      end
+    end
+    return nil if name_hash = {}
 end
